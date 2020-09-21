2a542ecb7aa6dd416679d53545b66293148858e2
Alex Goodwin <hagoodwi@gmail.com>
2020-09-21 10:23:52 -0700

    add final passing test

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -5,8 +5,9 @@ def ping_pong(number)
   counter = 1
   string_number = number.to_i
   string_number.times() do
-    # Check for numbers divisible by 3
-    if counter%3 == 0
+    if (counter%3 == 0) & (counter%5 == 0)
+      new_array.push('ping-pong')
+    elsif counter%3 == 0
       new_array.push('ping')
     elsif counter%5 == 0
       new_array.push('pong')
@@ -18,4 +19,4 @@ def ping_pong(number)
   return new_array
 end
 
-print ping_pong(7) 
+print ping_pong(15) 
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -6,10 +6,20 @@ describe('#ping_pong') do
   #   expect(ping_pong(7)).to(eq([1,2,3,4,5,6,7]))
   # end
 
-  it("Replaces numbers in the output that are divisible by 3 with 'ping'") do
-    expect(ping_pong(7)).to(eq([1,2,'ping',4,5,'ping',7]))
+  # it("Replaces numbers in the output that are divisible by 3 with 'ping'") do
+  #   expect(ping_pong(7)).to(eq([1,2,'ping',4,5,'ping',7]))
+  # end
+
+  # it("Replaces numbers divisible by 5 with 'pong'") do
+  #   expect(ping_pong(7)).to(eq([1,2,'ping',4,'pong','ping',7]))
+  # end
+
+  it("Replaces numbers divisible by 3 and 5 with 'ping-pong") do
+    expect(ping_pong(15)).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
   end
+
 end
 
+  
 
 
